fe0dda36a5260d50dcd886b81dbae7861bef445e
jasnmar <jasnmar@gmail.com>
2024-03-29 13:55:30 -0400

    Push, Pop, Shift, Unshift

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
 for (let i=0; i<largeCountries.length; i++) {
     console.log("- " + largeCountries[i]);
 }
+
+
+largeCountries.pop();
+largeCountries.push("Pakistan");
+largeCountries.shift();
+largeCountries.unshift("China");
+
+console.log(largeCountries);
