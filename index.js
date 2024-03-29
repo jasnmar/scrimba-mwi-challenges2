48c71df8b7df22b7feb0e570f17a48bf777ee9cb
jasnmar <jasnmar@gmail.com>
2024-03-29 13:57:50 -0400

    Conditionals

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ largeCountries.shift();
 largeCountries.unshift("China");
 
 console.log(largeCountries);
+
+let dayOfMonth = 13
+let weekday = "Friday"
+
+// If it is Friday the 13th, log out this spooky face: 😱
+// Use the logical "AND operator" -> &&
+
+if(dayOfMonth===13&&weekday==="Friday") {
+    console.log("😱")
+}
+
