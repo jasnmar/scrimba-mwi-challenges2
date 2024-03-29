79a63c2d24a39af6ef8b9ce40a471ec13a30c598
jasnmar <jasnmar@gmail.com>
2024-03-29 14:09:41 -0400

    FruitSorter

--- index.js
+++ index.js
@@ -1,50 +1,18 @@
-let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
 
-for (let i=0; i<largeCountries.length; i++) {
-    console.log("- " + largeCountries[i]);
+// Create a function that puts the apples onto the appleShelf
+// and the oranges onto the orangeShelf. Use a for loop,
+// a conditional statement, and the textContent property.
+
+function sort() {
+    for (let i = 0; i< fruit.length; i++) {
+        if (fruit[i]==="🍎") {
+            appleShelf.textContent += fruit[i]
+        } else {
+            orangeShelf.textContent += fruit[i]
+        }
+    }
 }
-
-
-largeCountries.pop();
-largeCountries.push("Pakistan");
-largeCountries.shift();
-largeCountries.unshift("China");
-
-console.log(largeCountries);
-
-let dayOfMonth = 13
-let weekday = "Friday"
-
-// If it is Friday the 13th, log out this spooky face: 😱
-// Use the logical "AND operator" -> &&
-
-if(dayOfMonth===13&&weekday==="Friday") {
-    console.log("😱")
-}
-
-let hands = ["rock", "paper", "scissor"]
-
-// Create a function that returns a random item from the array
-
-function rps() {
-    let number = Math.floor( Math.random()*3);
-    return hands[number]
-}
-
-console.log(rps())
-
-
-let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
-
-let stageEl = document.getElementById("stage")
-let fightButton = document.getElementById("fightButton")
-
-fightButton.addEventListener("click", function() {
-    // Challenge:
-    // When the user clicks on the "Pick Fighters" button, pick two random 
-    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
-    const fighter1Number = Math.floor( Math.random() * fighters.length);
-    const fighter2Number = Math.floor( Math.random() * fighters.length);
-    stageEl.textContent = fighters[fighter1Number] + " vs " + fighters[fighter2Number]
-      
-})
+sort();
